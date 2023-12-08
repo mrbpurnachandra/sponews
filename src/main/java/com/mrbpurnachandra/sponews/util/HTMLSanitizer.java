9fe8218858f6491c72da01af3eab849abcf5f4b5
Prakash Bhattarai <lightvelocity999@gmail.com>
2023-12-08 09:16:52 +0545

    Fixed bug which prevented bold and italic tags to be stripped

--- src/main/java/com/mrbpurnachandra/sponews/util/HTMLSanitizer.java
+++ src/main/java/com/mrbpurnachandra/sponews/util/HTMLSanitizer.java
@@ -5,7 +5,7 @@ import org.owasp.html.PolicyFactory;
 
 public class HTMLSanitizer {
     private static final PolicyFactory policy = new HtmlPolicyBuilder()
-            .allowElements("b", "i", "u", "s", "p", "blockquote", "ol", "ul", "li", "h3", "h4")
+            .allowElements("strong", "em", "u", "s", "p", "blockquote", "ol", "ul", "li", "h3", "h4")
             .toFactory();
 
     public static String sanitize(String html) {

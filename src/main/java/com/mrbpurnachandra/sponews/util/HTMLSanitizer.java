d5320633d3509665d2fe78ce61bb705583744c92
Prakash Bhattarai <lightvelocity999@gmail.com>
2024-02-06 17:10:05 +0545

    Changed content editor in favour of markdown editor and made necessary changes in HTMLSanitizer

--- src/main/java/com/mrbpurnachandra/sponews/util/HTMLSanitizer.java
+++ src/main/java/com/mrbpurnachandra/sponews/util/HTMLSanitizer.java
@@ -5,7 +5,7 @@ import org.owasp.html.PolicyFactory;
 
 public class HTMLSanitizer {
     private static final PolicyFactory policy = new HtmlPolicyBuilder()
-            .allowElements("strong", "em", "u", "s", "p", "blockquote", "ol", "ul", "li", "h3", "h4")
+            .allowElements("strong", "em", "u", "del", "p", "blockquote", "ol", "ul", "li", "h2", "h3")
             .toFactory();
 
     public static String sanitize(String html) {

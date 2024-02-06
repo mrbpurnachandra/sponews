d63092212e8c7304ef717ccccbb06448fee9b401
Prakash Bhattarai <lightvelocity999@gmail.com>
2024-02-06 17:56:38 +0545

    Implemented feature to include external images

--- src/main/java/com/mrbpurnachandra/sponews/util/HTMLSanitizer.java
+++ src/main/java/com/mrbpurnachandra/sponews/util/HTMLSanitizer.java
@@ -5,7 +5,9 @@ import org.owasp.html.PolicyFactory;
 
 public class HTMLSanitizer {
     private static final PolicyFactory policy = new HtmlPolicyBuilder()
-            .allowElements("strong", "em", "u", "del", "p", "blockquote", "ol", "ul", "li", "h2", "h3")
+            .allowElements("strong", "em", "u", "del", "p", "blockquote", "ol", "ul", "li", "h2", "h3", "img")
+            .allowAttributes("src", "alt").onElements("img")
+            .allowStandardUrlProtocols()
             .toFactory();
 
     public static String sanitize(String html) {
